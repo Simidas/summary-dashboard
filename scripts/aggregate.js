f505e62d19f16beb82acfa651496a424b35f1199
Weldon <443516438@qq.com>
2026-07-03 08:29:20 +0800

    回滚到c2f8045版本

--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -15,12 +15,8 @@
  * - data/summaries/insights/weekly/*.json
  */
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const fs = require('fs');
+const path = require('path');
 
 // Directories
 const ROOT_DIR = path.join(__dirname, '..');
